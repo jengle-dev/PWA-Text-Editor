832b4e23f4d2e28a730cd725996b6359c1c101ef
jengle-dev <j.engle.dev@gmail.com>
2023-04-26 21:46:15 -0500

    updates to service worker and config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
 
-    module: {
-      rules: [
-        
-      ],
+        new GenerateSW(),
+        new WebpackPwaManifest({
+          // TODO: Create a manifest.json:
+          name: 'My Progressive Web App',
+          short_name: 'MyPWA',
+          description: 'My awesome Progressive Web App!',
+          background_color: '#ffffff',
+          crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+          icons: [
+            {
+              src: path.resolve('src/assets/icon.png'),
+              sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            },
+            {
+              src: path.resolve('src/assets/large-icon.png'),
+              size: '1024x1024' // you can also use the specifications pattern
+            },
+            {
+              src: path.resolve('src/assets/maskable-icon.png'),
+              size: '1024x1024',
+              purpose: 'maskable'
+            }     
+    ]
+  }),
+],
+
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+        },
+      },
+    },
+  ],
+},
   };
 };
